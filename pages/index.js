bb1574d46299ebdaab0b4c7f6b8fd9a5cb06ad85
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-06 14:36:10 -0300

    Contentful Client

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
-export default function Recipes() {
-  return (
-    <div className="recipe-list">
-      Recipe List
-    </div>
-  )
+import { createClient } from 'contentful';
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  });
+
+  const response = await client.getEntries({ content_type: 'recipe' });
+
+  return {
+    props: {
+      recipes: response.items,
+    },
+  };
+}
+
+export default function Recipes({ recipes }) {
+  console.log(recipes);
+
+  return <div className="recipe-list">Recipe List</div>;
 }
