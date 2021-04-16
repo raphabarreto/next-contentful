e5fd2a8d5f75e0360693ed5b3a29bdccda58bd71
Raphael Barreto <tnt.raphael@gmail.com>
2021-04-16 15:46:34 -0300

    Conditional Redirects

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -31,6 +31,13 @@ export async function getStaticProps({ params }) {
     'fields.slug': params.slug,
   });
 
+  if (!items.length) {
+    return {
+      redirect: { destination: '/' },
+      permanent: false,
+    };
+  }
+
   return {
     props: { recipe: items[0] },
     revalidate: 10,
